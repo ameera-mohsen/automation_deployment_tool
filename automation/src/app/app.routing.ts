d2c2c08b75f6342644650895bec2e08e19df3686
Ameera <ameera.mohsen@gmail.com>
2019-04-09 14:38:18 +0200

    Initial version

---
+++ automation/src/app/app.routing.ts
@@ -0,0 +1,18 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { AuthGuard } from './_guards';
+import { NewDeploymentComponent } from './new-deployment/new-deployment.component';
+
+const appRoutes: Routes = [
+ //   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'newRequest', component: NewDeploymentComponent}
+    // otherwise redirect to home
+  //  { path: '**', redirectTo: '' }
+]; 
+
+export const routing = RouterModule.forRoot(appRoutes);
