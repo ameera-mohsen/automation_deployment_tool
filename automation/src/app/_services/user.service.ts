a72d893460c9bdbf3b4b341e94d8542806041606
Mohamed <mohamed.sam.mohamed@dxc.com>
2019-04-14 18:18:00 +0200

    fix login issue and space issue and download the common lib

--- automation/src/app/_services/user.service.ts
+++ automation/src/app/_services/user.service.ts
@@ -14,7 +14,7 @@ export class UserService {
 
 
     login(userCred: UserCredentials): Observable<any> {
-        return this.http.post(`http://localhost:8099/api/login` , userCred);
+        return this.http.post(`http://localhost:8076/api/login` , userCred);
     }
 
 
